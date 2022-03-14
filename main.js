642d933013c2c803aedcedc7506770e3b4542c0b
ErikaSarleti <erikasarleti00@gmail.com>
2022-03-14 15:44:28 +0100

    aggiunta array e struttura

---
+++ main.js
@@ -0,0 +1,42 @@
+/*
+Rifare l'esercizio della to do list.
+Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
+- text, una stringa che indica il testo del todo
+- done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+MILESTONE 2
+Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+MILESTONE 3
+Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+Bonus:
+1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa) 
+*/
+
+
+
+
+const app = new Vue({
+    el: '#app' ,
+    data: {
+        todoList: [
+            {
+                text:'Fare l\'esercizio',
+                done: false,
+            },
+            {
+                text:'lavare i piatti',
+                done:false,
+            },
+            {
+                text:'fare la spesa',
+                done:false,
+            },
+        ]
+
+    },
+    methods: {
+        deleteTodo(index){
+            this.todoList.splice(this.todoList.length - 1, );
+        }
+    }
+})
